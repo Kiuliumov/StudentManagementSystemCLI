61005520f49ee03d41577f3fda14c920b4c55b16
Kiuliumov <miti.bania@gmail.com>
2026-02-12 10:06:30 +0200

    Update student.c

--- student.c
+++ student.c
@@ -42,3 +42,69 @@ void add_student()
 
     save_student(s);
 }
+
+void view_student()
+{
+    FILE *file = fopen("students.dat", "rb");
+    Student s;
+
+    if (!file)
+    {
+        printf("No students found.\n");
+        return;
+    }
+
+    printf("ID\tName\tAge\tGrade\n");
+    printf("-----------------------------------\n");
+
+    while (fread(&s, sizeof(Student), 1, file))
+    {
+        printf("%d\t%s\t%d\t%.2f\n", s.id, s.name, s.age, s.grade);
+    }
+
+    fclose(file);
+}
+
+void find_student_by_id()
+{
+    int id;
+    printf("Enter student ID to find: ");
+    scanf("%d", &id);
+
+    FILE *file = fopen("students.dat", "rb");
+    Student s;
+    int found = 0;
+
+    if (!file)
+    {
+        printf("No students found.\n");
+        return;
+    }
+
+    while (fread(&s, sizeof(Student), 1, file))
+    {
+        if (s.id == id)
+        {
+            printf("Student found:\n");
+            printf("ID: %d\nName: %s\nAge: %d\nGrade: %.2f\n", s.id, s.name, s.age, s.grade);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        printf("Student with ID %d not found.\n", id);
+    }
+
+    fclose(file);
+}
+
+void delete_student_by_id()
+{
+    int id;
+    printf("Enter student ID to delete: ");
+    scanf("%d", &id);
+
+    delete_student_from_file(id);
+}
